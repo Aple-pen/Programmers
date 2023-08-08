2b6d26d8dbba25c4640daacab0b1b79d7101b402
eunggikim <rubie3374@naver.com>
2023-08-08 18:47:56 +0900

    1-7 카드 뭉치 문제 풀이중..

--- main.cpp
+++ main.cpp
@@ -14,38 +14,55 @@ void vectorPrint(vector<T> v) {
     cout << endl;
 }
 
-vector<int> solution(vector<string> keymap, vector<string> targets) {
-    unordered_map<char,int> keyHash;
-    vector<int>answer;
-    for(auto key : keymap){
-        for(int i = 0; i<key.length();i++){
-            auto iter = keyHash.find(key[i]);
-            if(iter != keyHash.end()){
-                if(i + 1 < iter->second )iter->second = i + 1;
-            }else keyHash.insert(make_pair(key[i],i + 1));
-        }
+string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
+    string answer = "Yes";
+    int prevIndex = 0;
+    unordered_map <string,int> hash_goal(goal.size());
+
+    for(int i = 0;i<goal.size();i++){
+        hash_goal.insert(make_pair( goal[i],i));
     }
 
-    for(auto target : targets){
-        int sum = 0;
-        for(auto targetChar : target){
-            auto iter = keyHash.find(targetChar);
-            if(iter != keyHash.end()) sum += iter->second;
-            else{
-                sum = -1;
-                break;
+    for(int i =0 ;i<cards1.size();i++){
+        auto r = hash_goal.find(cards1[i]);
+        if(r != hash_goal.end()){
+            if(r->second >= prevIndex){
+                prevIndex = r->second;
+                answer = "Yes";
             }
+            else return "No";
+        }else{
+            answer = "No";
         }
-        answer.push_back(sum == 0 ? -1 : sum);
     }
 
+    prevIndex = 0;
+
+    for(int i =0 ;i<cards2.size();i++){
+        auto r = hash_goal.find(cards2[i]);
+        if(r != hash_goal.end()){
+            if(r->second >= prevIndex) {
+                prevIndex = r->second;
+                answer = "Yes";
+            }
+            else return "No";
+        }else{
+            answer = "No";
+        }
+    }
+
+    if(prevIndex == 0) answer = "Yes";
     return answer;
 }
 
 
 int main() {
-    vector<string> keymap = {"ABACD", "BCEFD"};
-    vector<string> target = {"ABCD","AABB"};
-    vectorPrint(solution(keymap, target));
+    vector<string> keymap = {"def", "bcd"};
+    vector<string> target = {"b", "c", "d"};
+    vector<string> goal = {"bcd"};
+//    vector<string> keymap = {"i", "drink", "water", "wow"};
+//    vector<string> target = {"want", "to"};
+//    vector<string> goal = {"i", "want", "to", "drink", "water"};
+    cout<<solution(keymap,target,goal)<<endl;
     return 0;
 }
